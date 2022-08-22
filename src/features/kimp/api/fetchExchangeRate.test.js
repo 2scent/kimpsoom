d9380364c81479472186ddcc3680c5061032502c
2scent <ad7549@gmail.com>
2022-08-22 16:35:08 +0900

    Test real api

--- src/features/kimp/api/fetchExchangeRate.test.js
+++ src/features/kimp/api/fetchExchangeRate.test.js
@@ -1,19 +1,9 @@
-import axios from 'axios';
-
 import fetchExchangeRate from './fetchExchangeRate';
 
-jest.mock('axios');
-
 describe('fetchExchangeRate', () => {
-  const basePrice = 1312.00;
-
-  beforeEach(() => {
-    axios.get.mockResolvedValue({ data: [{ basePrice }] });
-  });
-
   it('returns exchange rate', async () => {
     const exchangeRate = await fetchExchangeRate();
 
-    expect(exchangeRate).toBe(basePrice);
+    expect(typeof exchangeRate).toBe('number');
   });
 });
--- src/features/kimp/api/fetchUpbitTickers.test.js
+++ src/features/kimp/api/fetchUpbitTickers.test.js
@@ -1,24 +1,9 @@
-import axios from 'axios';
-
-import UPBIT_COINS from '@fixtures/upbit-coins';
-
 import fetchUpbitTickers from './fetchUpbitTickers';
 
-jest.mock('axios');
-
 describe('fetchUpbitTickers', () => {
-  beforeEach(() => {
-    axios.get.mockResolvedValue({ data: UPBIT_COINS });
-  });
-
   it('returns upbit coins', async () => {
-    const upbitCoins = await fetchUpbitTickers();
+    const tickers = await fetchUpbitTickers();
 
-    expect(upbitCoins).toEqual(
-      UPBIT_COINS
-        .filter((coin) => coin.market.startsWith('KRW'))
-        .map((coin) => coin.market)
-        .map((market) => market.split('-')[1]),
-    );
+    tickers.forEach((ticker) => expect(typeof ticker).toBe('string'));
   });
 });
