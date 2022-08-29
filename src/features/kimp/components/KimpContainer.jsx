df1f44f0e580e6c961c5965a7a4ae3a4f939bd62
2scent <ad7549@gmail.com>
2022-08-29 16:26:48 +0900

    test 1

--- src/features/kimp/components/KimpContainer.jsx
+++ src/features/kimp/components/KimpContainer.jsx
@@ -1,9 +1,50 @@
+import { Button, Grid } from '@mui/material';
+import { useState } from 'react';
 import useUpbitTickers from '../hooks/useUpbitTickers';
 
 import KimpList from './KimpList';
 
+function Tickers({
+  tickers,
+  selectedTickers,
+  onClickTicker,
+}) {
+  return (
+    <Grid container spacing={1}>
+      {tickers
+        .map((ticker) => (
+          <Grid
+            key={ticker}
+            item
+            xs={1}
+          >
+            <Button onClick={() => onClickTicker(ticker)}>
+              {ticker}
+              {selectedTickers.includes(ticker) ? ' (V)' : ''}
+            </Button>
+          </Grid>
+        ))}
+    </Grid>
+  );
+}
+
 function KimpContainer() {
-  const { isLoading, data: tickers } = useUpbitTickers();
+  const [selectedTickers, setSelectedTickers] = useState([]);
+  const { isLoading, data: tickers } = useUpbitTickers({
+    onSuccess: setSelectedTickers,
+  });
+
+  const handleClickTicker = (ticker) => {
+    if (selectedTickers.includes(ticker)) {
+      setSelectedTickers(
+        selectedTickers
+          .filter((selectedTicker) => selectedTicker !== ticker),
+      );
+      return;
+    }
+
+    setSelectedTickers([...selectedTickers, ticker]);
+  };
 
   return (
     <>
@@ -11,9 +52,18 @@ function KimpContainer() {
       {isLoading
         ? <p>로딩 중</p>
         : (
-          <KimpList
-            tickers={tickers}
-          />
+          <>
+            <Tickers
+              tickers={tickers}
+              selectedTickers={selectedTickers}
+              onClickTicker={handleClickTicker}
+            />
+            <KimpList
+              tickers={
+                tickers.filter((ticker) => selectedTickers.includes(ticker))
+              }
+            />
+          </>
         )}
     </>
   );
--- src/features/kimp/hooks/useUpbitTickers.js
+++ src/features/kimp/hooks/useUpbitTickers.js
@@ -2,9 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 
 import fetchUpbitTickers from '../api/fetchUpbitTickers';
 
-export default function useUpbitKrwTickers() {
+export default function useUpbitKrwTickers({ onSuccess }) {
   return useQuery(
     ['upbit', 'tickers', 'krw'],
     fetchUpbitTickers,
+    { onSuccess },
   );
 }
