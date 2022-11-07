e0a20d902f817bc8411afc7cc796158e50749545
2scent <ad7549@gmail.com>
2022-11-07 11:17:39 +0900

    Add tests for useThrottle

---
+++ src/shared/hooks/useThrottle.test.ts
@@ -0,0 +1,46 @@
+import { renderHook } from '@testing-library/react';
+
+import useThrottle from './useThrottle';
+
+jest.useFakeTimers();
+
+describe('useThrottle', () => {
+  const cb = jest.fn();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+  });
+
+  it('executes callback at most once per delay', () => {
+    const { result: { current: throttle } } = renderHook(() => useThrottle(500));
+
+    throttle('key', cb);
+    throttle('key', cb);
+    throttle('key', cb);
+    throttle('key', cb);
+
+    jest.advanceTimersByTime(500);
+
+    throttle('key', cb);
+    throttle('key', cb);
+    throttle('key', cb);
+
+    jest.advanceTimersByTime(500);
+
+    throttle('key', cb);
+    throttle('key', cb);
+
+    expect(cb).toHaveBeenCalledTimes(3);
+  });
+
+  it('applies delay for each key', () => {
+    const { result: { current: throttle } } = renderHook(() => useThrottle(500));
+
+    throttle('key1', cb);
+    throttle('key2', cb);
+    throttle('key3', cb);
+
+    expect(cb).toHaveBeenCalledTimes(3);
+  });
+});
