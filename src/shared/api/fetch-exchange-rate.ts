45ef7fe465b9118f9ac78fd646ecd686af08b49a
2scent <ad7549@gmail.com>
2024-10-16 00:39:14 +0900

    Replace exchange rate API with mock dat

--- src/shared/api/fetch-exchange-rate.ts
+++ src/shared/api/fetch-exchange-rate.ts
@@ -1,9 +1,4 @@
-import axios from 'axios';
-
 export default async function fetchExchangeRate(): Promise<number> {
-  const url = 'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD';
-
-  const response = await axios.get(url);
-
-  return response.data[0].basePrice;
+  // 이용 가능한 환율 API를 찾을 수 없어 임시 값으로 대체
+  return 1360;
 }
