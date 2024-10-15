3800867821e3b96243cda3c37612180c9f9fec7e
2scent <ad7549@gmail.com>
2024-10-15 23:27:20 +0900

    Remove unnecessary fetch methods

--- src/features/kimp/api/fetch-exchange-rate.test.ts
+++
@@ -1,9 +0,0 @@
-import fetchExchangeRate from './fetch-exchange-rate';
-
-describe('fetchExchangeRate', () => {
-  it('returns exchange rate', async () => {
-    const exchangeRate = await fetchExchangeRate();
-
-    expect(typeof exchangeRate).toBe('number');
-  });
-});
--- src/features/kimp/api/fetch-exchange-rate.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default async function fetchExchangeRate(): Promise<number> {
-  const url = 'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD';
-
-  const response = await axios.get(url);
-
-  return response.data[0].basePrice;
-}
